eba44902d70831db829dc0d36d58259164128707
해명 <hmyeonge@gmail.com>
2025-06-03 20:34:26 +0900

    chore: vite-plugin-pwa 설치 및 config 옵션 적용 / 홈 화면 아이콘 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // 서비스 워커 자동 업데이트
+      injectRegister: 'auto', // 서비스 워커 자동 등록
+      manifest: {
+        name: 'PLOG',
+        short_name: 'PLOG',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        // 192, 512 해상도에서 홈 화면 아이콘 지원
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
