8e71e67ac3b41094da907ce9e0141e4f82c04c85
해명 <hmyeonge@gmail.com>
2025-07-01 23:12:22 +0900

    feat: 공통 레이아웃(MainLayout) 여부에 따라 인증/온보딩과 메인 서비스 라우트 분리

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import StyleGuide from './pages/StyleGuide';
+import LoginPage from './pages/login';
+import OnBoarding from './pages/onboarding';
+import MainLayout from './components/MainLayout';
 
 const Router = () => {
     return (
         <Routes>
+            {/* 인증/온보딩 : 푸터 없는 페이지들 */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/onboarding" element={<OnBoarding />} />
             <Route path="/style-guide" element={<StyleGuide />} />
+
+            {/* 메인 서비스 : 푸터 있는 페이지들 */}
+            <Route path="/" element={<MainLayout />} />
         </Routes>
     );
 };
