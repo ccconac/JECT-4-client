903e6438d003a672fec1db4200925d5c6768ea02
해명 <hmyeonge@gmail.com>
2025-07-04 15:39:37 +0900

    feat: 코드 리뷰 반영 - 라우팅 추가

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,9 @@ import LoginPage from './pages/auth/LoginPage/KakaoLoginPage';
 import LoginAccessPage from './pages/auth/LoginPage/KakaoLoginAccessPage';
 import SetNamePage from './pages/setup/setProfile/SetNamePage';
 import SetJobPage from './pages/setup/setProfile/SetJobPage';
+import CreateGoalPage from './pages/setup/setTravelGoal/createGoal';
+import SetGoalTypePage from './pages/setup/setTravelGoal/setGoalType';
+import SetMileStonePage from './pages/setup/setTravelGoal/setMileStone';
 import MainLayout from './components/MainLayout';
 import StyleGuide from './pages/style-guide/StyleGuide';
 
@@ -14,13 +17,14 @@ const Router = () => {
             <Route path="/oauth/callback/kakao" element={<LoginAccessPage />} />
             <Route path="/setName" element={<SetNamePage />} />
             <Route path="/setJob" element={<SetJobPage />} />
+            <Route path="/createGoal" element={<CreateGoalPage />} />
+            <Route path="/setGoalType" element={<SetGoalTypePage />} />
+            <Route path="/setMileStone" element={<SetMileStonePage />} />
 
             {/* 스타일 가이드 페이지 */}
             <Route path="/style-guide" element={<StyleGuide />} />
-
             {/* 메인 서비스 : 푸터 있는 페이지들 */}
             <Route path="/" element={<MainLayout />} />
-
             {/* 404 처리 : 메인 페이지 리다이렉트 (임시) */}
             <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
