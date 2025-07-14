354dfb2763958a435d971133ceef7ec510e3cd5c
해명 <hmyeonge@gmail.com>
2025-07-14 12:50:54 +0900

    design: 카드 여백 및 텍스트 사이즈 조정

--- src/pages/setup/setTravelGoal/setTravelType/TravelTypeCard.tsx
+++ src/pages/setup/setTravelGoal/setTravelType/TravelTypeCard.tsx
@@ -9,13 +9,12 @@ type TravelTypeCardProps = {
 const TravelTypeCard = ({ title, description, recommend }: TravelTypeCardProps) => {
     // name 에 따라 title color 조건부 렌더링
     return (
-        <section className="rounded-xl bg-white px-10 py-7.5 shadow-md">
+        <section className="rounded-xl bg-white px-[39px] py-7.5 shadow-md">
             <h3 className="text-point2 text-display">{title}</h3>
             <p className="relative flex flex-col gap-3">
                 <span className="text-text-min text-[14px]">{description}</span>
                 <hr className="border-primary absolute top-1/2 right-0 left-0 border-0 border-t-[0.3px] border-solid" />
-                {/* 텍스트 오버플로우 방지 : text-[11px] 조정 */}
-                <span className="text-text-min text-[11px]">{recommend}</span>
+                <span className="text-text-min text-[12px]">{recommend}</span>
             </p>
         </section>
     );
