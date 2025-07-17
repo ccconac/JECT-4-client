5849a77a58e95b6712678f543d7a67f2c4679ffb
해명 <hmyeonge@gmail.com>
2025-07-17 11:58:33 +0900

    feat: 헤더에 뒤로가기 버튼 추가

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.454u9qgvg9o"
+    "revision": "0.e8dsm2g0auo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
