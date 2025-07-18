f568b7b578806d901f2f9b78c4c529e4363dece1
해명 <hmyeonge@gmail.com>
2025-07-18 12:12:50 +0900

    fix: 에러 해결

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,3 +4,15 @@ declare module 'swiper/css/navigation';
 declare module 'swiper/css/pagination';
 declare module 'swiper/css/scrollbar';
 declare module '*.png';
+
+// Vite 환경변수 타입 추가
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_KAKAO_REST_API_KEY: string;
+    readonly VITE_API_BASE_URL: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
