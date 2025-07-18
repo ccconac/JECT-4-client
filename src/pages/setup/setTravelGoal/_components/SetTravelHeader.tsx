53404945a6ee9115289fc11a65d496655d71baf7
해명 <hmyeonge@gmail.com>
2025-07-18 21:12:31 +0900

    feat: 화살표 클릭 시 뒤로가기 기능 추가

--- src/pages/setup/setTravelGoal/_components/SetTravelHeader.tsx
+++ src/pages/setup/setTravelGoal/_components/SetTravelHeader.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router';
 import arrowBackIcon from '../../../../assets/icons/arrow_back.svg';
 
 interface SetTravelHeaderProps {
@@ -6,13 +7,14 @@ interface SetTravelHeaderProps {
 }
 
 const SetTravelHeader = ({ title, description }: SetTravelHeaderProps) => {
+    const navigate = useNavigate();
     return (
         <div>
             <img
                 src={arrowBackIcon}
                 alt="뒤로 가기"
                 className="mb-[17px] cursor-pointer"
-                // 클릭 시 뒤로 가기 추가 필요
+                onClick={() => navigate(-1)}
             />
             <header className="flex flex-col gap-1">
                 <h1 className="text-secondary text-title">{title}</h1>
