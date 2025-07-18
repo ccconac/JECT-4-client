d8d31ca12127941abaad8e51125bb18d16937027
pjyoung0511 <doeob6@ajou.ac.kr>
2025-07-18 15:46:29 +0900

    fix: 빌드 에러 해결

--- src/pages/dashboard/MainPage.tsx
+++ src/pages/dashboard/MainPage.tsx
@@ -1,6 +1,4 @@
 // 셋업 이후 메인 화면
-import { useNavigate, useSearchParams } from 'react-router';
-import { useEffect } from 'react';
 import MainTabButton from './MainTabButton';
 
 const MainPage = () => {
