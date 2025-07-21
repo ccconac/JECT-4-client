5f633e0176ed4f24f364f30ed2cd2a6596b62d6b
해명 <hmyeonge@gmail.com>
2025-07-21 17:16:25 +0900

    feat: / 루트 경로 KakaoLoginPage 로 수정

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,16 +5,18 @@ import SetNamePage from './pages/setup/setProfile/SetNamePage';
 import SetJobPage from './pages/setup/setProfile/SetJobPage';
 
 import MainPage from './pages/dashboard/MainPage';
-import MainLayout from './components/MainLayout';
 import StyleGuide from './pages/style-guide/StyleGuide';
 
 import SetTravelTypePage from './pages/setup/setTravelGoal/setTravelType/index';
 import CreateTravelPage from './pages/setup/setTravelGoal/createTravel';
 import CreateStampPage from './pages/setup/setTravelGoal/createStamp';
+import KakaoLoginPage from './pages/auth/LoginPage/KakaoLoginPage';
 
 const Router = () => {
     return (
         <Routes>
+            {/* 메인 서비스 : 푸터 있는 페이지들 */}
+            <Route path="/" element={<KakaoLoginPage />} />
             {/* 온보딩, 로그인, 셋업 페이지 */}
             <Route path="/login" element={<LoginPage />} />
             <Route path="/oauth/callback/kakao" element={<LoginAccessPage />} />
@@ -34,8 +36,6 @@ const Router = () => {
 
             {/* 스타일 가이드 페이지 */}
             <Route path="/style-guide" element={<StyleGuide />} />
-            {/* 메인 서비스 : 푸터 있는 페이지들 */}
-            <Route path="/" element={<MainLayout />} />
             {/* 404 처리 : 메인 페이지 리다이렉트 (임시) */}
             <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
