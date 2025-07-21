bce9fa7dc57cfd7b6fb25cc576b49c257126599c
해명 <hmyeonge@gmail.com>
2025-07-21 12:50:01 +0900

    design: 도착일 필드에 달력 아이콘 추가

--- src/pages/setup/setTravelGoal/createTravel/SetTravelDateInput.tsx
+++ src/pages/setup/setTravelGoal/createTravel/SetTravelDateInput.tsx
@@ -1,7 +1,8 @@
 import dayjs from 'dayjs';
+import CalendarIcon from '../../../../assets/icons/calendar_icon.svg';
 
 interface SetTravelDateInputProps {
-    labelName: string;
+    labelName: 'startDate' | 'finishDate';
 }
 
 const SetTravelDateInput = ({ labelName }: SetTravelDateInputProps) => {
@@ -13,7 +14,6 @@ const SetTravelDateInput = ({ labelName }: SetTravelDateInputProps) => {
     };
 
     const currentDate = dayjs();
-    console.log(currentDate.format('YYYY년 M월 D일'));
 
     return (
         <div className="flex flex-col gap-[7px]">
@@ -26,12 +26,19 @@ const SetTravelDateInput = ({ labelName }: SetTravelDateInputProps) => {
                     : labelMap.finishDate}
                 <span className="text-point1"> *</span>
             </label>
-            <input
-                className="placeholder:text-subtitle placeholder:text-text-min border-input-sub bg-input-focus h-[50px] rounded-md border py-2.5 pl-[15px] placeholder:opacity-40"
-                placeholder={currentDate.format('YYYY년 M월 D일')}
-                id="date"
-                disabled={labelName === 'startDate'}
-            />
+            <div className="relative w-full">
+                <input
+                    className="placeholder:text-subtitle placeholder:text-text-min border-input-sub bg-input-focus h-[50px] w-full rounded-md border py-2.5 pr-10 pl-[15px] placeholder:opacity-40"
+                    placeholder={currentDate.format('YYYY년 M월 D일')}
+                    id="date"
+                    disabled={labelName === 'startDate'}
+                />
+                {labelName === 'finishDate' && (
+                    <button className="absolute top-1/2 right-3 -translate-y-1/2 transform cursor-pointer">
+                        <img src={CalendarIcon} alt="calendar" />
+                    </button>
+                )}
+            </div>
         </div>
     );
 };
