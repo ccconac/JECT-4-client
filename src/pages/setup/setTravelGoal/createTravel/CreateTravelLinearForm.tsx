8d3c819d59eec6a65322f6b36edc63187c7364ce
해명 <hmyeonge@gmail.com>
2025-07-23 15:38:08 +0900

    chore: Node 내장 모듈 사용을 위한 @types/node 설치

--- src/pages/setup/setTravelGoal/createTravel/CreateTravelLinearForm.tsx
+++ src/pages/setup/setTravelGoal/createTravel/CreateTravelLinearForm.tsx
@@ -1,39 +1,54 @@
+import NextButton from '../../../../components/common/button/NextButton';
 import SetTravelDateInput from './SetTravelDateInput';
 import SetTravelMemoInput from './SetTravelMemoInput';
 import SetTravelNameInput from './SetTravelNameInput';
-import SetFinishDateModal from '../_components/SetFinishDateModal';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+type FormValues = {
+    startDate: string;
+    finishDate: string;
+    name: string;
+    memo: string;
+};
 
 const CreateTravelLinearForm = () => {
+    // modal
     const [isOpen, setIsOpen] = useState(false);
 
     const handleModalOpen = () => {
         setIsOpen(!isOpen);
-        console.log('modal open');
+    };
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormValues>();
+
+    const onSubmit = (data: FormValues) => {
+        console.log(data);
     };
 
     return (
         <div>
-            <form className="mt-[46px] flex flex-col gap-[7px]">
+            <form
+                className="mt-[46px] flex flex-col gap-[7px]"
+                onSubmit={handleSubmit(onSubmit)}
+            >
                 <SetTravelDateInput labelName="startDate" />
                 <SetTravelDateInput
                     labelName="finishDate"
                     onModalToggle={handleModalOpen}
+                    register={register}
                 />
                 <span className="text-point1 text-small">
                     * 필수로 입력해주세요.{' '}
                 </span>
-                <SetTravelNameInput />
-                <SetTravelMemoInput />
-
-                <button
-                    className="bg-text-min mx-auto mt-[94px] flex h-[51px] w-[350px] cursor-pointer flex-row items-center justify-center gap-[10px] rounded-md px-[161px] py-4 font-medium text-white"
-                    type="submit"
-                >
-                    다음
-                </button>
+                <SetTravelNameInput register={register} />
+                <SetTravelMemoInput register={register} />
+                <NextButton type="submit" />
             </form>
-            {isOpen && <SetFinishDateModal />}
         </div>
     );
 };
