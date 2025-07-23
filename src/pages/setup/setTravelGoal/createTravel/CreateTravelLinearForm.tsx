3c14fe5d3e2edf50aa5d99f08dadb3669d712459
해명 <hmyeonge@gmail.com>
2025-07-23 16:02:18 +0900

    chore: 절대 경로 설정

--- src/pages/setup/setTravelGoal/createTravel/CreateTravelLinearForm.tsx
+++ src/pages/setup/setTravelGoal/createTravel/CreateTravelLinearForm.tsx
@@ -1,56 +1,9 @@
-import NextButton from '../../../../components/common/button/NextButton';
-import SetTravelDateInput from './SetTravelDateInput';
-import SetTravelMemoInput from './SetTravelMemoInput';
-import SetTravelNameInput from './SetTravelNameInput';
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-
-type FormValues = {
-    startDate: string;
-    finishDate: string;
-    name: string;
-    memo: string;
-};
-
 const CreateTravelLinearForm = () => {
-    // modal
-    const [isOpen, setIsOpen] = useState(false);
-
-    const handleModalOpen = () => {
-        setIsOpen(!isOpen);
-    };
-
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<FormValues>();
-
-    const onSubmit = (data: FormValues) => {
-        console.log(data);
-    };
-
-    return (
+    return(
         <div>
-            <form
-                className="mt-[46px] flex flex-col gap-[7px]"
-                onSubmit={handleSubmit(onSubmit)}
-            >
-                <SetTravelDateInput labelName="startDate" />
-                <SetTravelDateInput
-                    labelName="finishDate"
-                    onModalToggle={handleModalOpen}
-                    register={register}
-                />
-                <span className="text-point1 text-small">
-                    * 필수로 입력해주세요.{' '}
-                </span>
-                <SetTravelNameInput register={register} />
-                <SetTravelMemoInput register={register} />
-                <NextButton type="submit" />
-            </form>
-        </div>
-    );
-};
 
-export default CreateTravelLinearForm;
+        </div>
+    )
+}
+
+export default CreateTravelLinearForm
