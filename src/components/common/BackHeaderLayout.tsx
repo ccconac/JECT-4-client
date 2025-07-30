f2eb75f8324f309ff4c8117ce0e927a1f7d1ca8f
pjyoung0511 <pjiyoung4512@gmail.com>
2025-07-30 15:47:23 +0900

    feat : SCRUM-58 - 뽀모도로 타이머 미션 카드 구현

--- src/components/common/BackHeaderLayout.tsx
+++ src/components/common/BackHeaderLayout.tsx
@@ -4,9 +4,14 @@ import { useNavigate } from 'react-router';
 interface BackHeaderProps {
     title: string;
     onBack?: () => void; // 뒤로가기 동작 커스터마이징 가능
+    hideBackButton?: boolean; // 뒤로가기 버튼 보일지 결정
 }
 
-const BackHeader = ({ title, onBack }: BackHeaderProps) => {
+const BackHeader = ({
+    title,
+    onBack,
+    hideBackButton = false,
+}: BackHeaderProps) => {
     const navigate = useNavigate();
 
     const handleBack = () => {
@@ -19,9 +24,13 @@ const BackHeader = ({ title, onBack }: BackHeaderProps) => {
 
     return (
         <div className="absolute inset-x-0 flex w-full items-center px-4 py-6">
-            <button onClick={handleBack}>
-                <LeftArrow className="h-4 w-4" />
-            </button>
+            {hideBackButton ? (
+                <div className="h-4 w-4" /> // 왼쪽 공간 맞추기용
+            ) : (
+                <button onClick={handleBack}>
+                    <LeftArrow className="h-4 w-4" />
+                </button>
+            )}
             <div className="text-text-sub text-subtitle flex-1 text-center font-semibold">
                 {title}
             </div>
---
+++ src/pages/pomodoro/PomodoroMissionModal.tsx
@@ -0,0 +1,30 @@
+import PomodoroMissionItem from './PomodoroMissionItem';
+
+type Props = {
+    isAutoStop: boolean;
+};
+
+const PomodoroMissionModal = ({ isAutoStop }: Props) => {
+    if (isAutoStop) {
+        return (
+            <div className="flex flex-col items-center">
+                <div className="text-title text-text-sub">25분 집중 완료!</div>
+                <div className="text-caption text-text-sub">
+                    이제 잠깐 숨을 돌릴 시간이에요. 5분 휴식하세요.
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="text-text-sub rounded-b-xl bg-white px-7 py-4 shadow-[0_8px_12px_rgba(168,168,168,0.12)]">
+            <div className="pb-4 text-[18px] font-semibold">제목</div>
+            <div className="text-body flex flex-col gap-4">
+                <PomodoroMissionItem />
+                <PomodoroMissionItem />
+            </div>
+        </div>
+    );
+};
+
+export default PomodoroMissionModal;
