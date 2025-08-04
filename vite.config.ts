854a8e7597bffa6d01ad1d4a9cbbb0601a44c987
pjyoung0511 <pjiyoung4512@gmail.com>
2025-08-04 17:15:46 +0900

    chore : 앱 아이콘 추가

--- vite.config.ts
+++ vite.config.ts
@@ -51,12 +51,12 @@ export default defineConfig({
                 // 192, 512 해상도에서 홈 화면 아이콘 지원
                 icons: [
                     {
-                        src: '/icons/icon-192x192.png',
+                        src: '/public/assets/icons/icon-192x192.png',
                         sizes: '192x192',
                         type: 'image/png',
                     },
                     {
-                        src: '/icons/icon-512x512.png',
+                        src: '/public/assets/icons/icon-512x512.png',
                         sizes: '512x512',
                         type: 'image/png',
                     },
