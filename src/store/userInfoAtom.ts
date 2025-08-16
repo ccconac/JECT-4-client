f736504355c7d05f418dd58467600d2bc2638b26
pjyoung0511 <pjiyoung4512@gmail.com>
2025-08-16 12:26:19 +0900

    fix : 유저 이름 수정 로직 추가

--- src/store/userInfoAtom.ts
+++ src/store/userInfoAtom.ts
@@ -5,16 +5,24 @@ export const memberNameAtom = atom<string>('');
 export const memberLoadingAtom = atom<boolean>(false);
 export const memberErrorAtom = atom<string | null>(null);
 
-export const fetchMemberNameAtom = atom(null, async (_get, set) => {
-    set(memberLoadingAtom, true);
-    set(memberErrorAtom, null);
+export const fetchMemberNameAtom = atom(
+    null,
+    async (_get, set, newName?: string) => {
+        set(memberLoadingAtom, true);
+        set(memberErrorAtom, null);
 
-    try {
-        const data = await getUserName();
-        set(memberNameAtom, data);
-    } catch (error) {
-        set(memberErrorAtom, '유저 정보를 불러오지 못했습니다.');
-    } finally {
-        set(memberLoadingAtom, false);
+        try {
+            let data;
+            if (newName) {
+                data = newName; // 인자가 있으면 바로 세팅
+            } else {
+                data = await getUserName(); // 없으면 API 호출
+            }
+            set(memberNameAtom, data);
+        } catch (error) {
+            set(memberErrorAtom, '유저 정보를 불러오지 못했습니다.');
+        } finally {
+            set(memberLoadingAtom, false);
+        }
     }
-});
+);
